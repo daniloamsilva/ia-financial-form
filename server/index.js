8dfd1f9d118b31484b6234af666c06ed087c7441
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2025-09-15 21:41:22 +0000

    chore: initialize server package with basic configuration and dependencies

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('🚀 Server is running');
+});
